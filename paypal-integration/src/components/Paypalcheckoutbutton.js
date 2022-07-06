65c971185a4d62b02409ed341048c6818937e7a5
Aradhya-Patro <82884623+Aradhyapatro@users.noreply.github.com>
2022-07-06 14:49:25 +0530

    switching

--- paypal-integration/src/components/Paypalcheckoutbutton.js
+++ paypal-integration/src/components/Paypalcheckoutbutton.js
@@ -1,26 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { PayPalScriptProvider, PayPalButtons } from '@paypal/react-paypal-js';
 
 const Paypalcheckoutbutton = ({ product }) => {
+    const [paidFor, setPaidFor] = useState(false)
+    const [err, setErr] = useState(null);
+
     const handleApprove = (orderID) => {
         console.log(orderID);
+        setPaidFor(true);
     }
 
+    if (paidFor) {
+        alert("Thank you for buying from us")
+    }
+
+    if (err) {
+        alert(err);
+    }
 
     return (
         <PayPalScriptProvider>
-            <PayPalButtons createOrder={(data, actions) => {
-                return actions.order.create({
-                    purchase_units: [
-                        {
-                            description: product.description,
-                            amount: {
-                                value: product.amount
+            <PayPalButtons
+                onClick={(data, actions) => {
+                    const hasAlreadyBoughtCourse = false;
+                    if (hasAlreadyBoughtCourse) {
+                        setErr("You Already bough this course");
+                        return actions.reject();
+                    } else {
+                        return actions.resolve();
+                    }
+                }}
+                createOrder={(data, actions) => {
+                    return actions.order.create({
+                        purchase_units: [
+                            {
+                                description: product.description,
+                                amount: {
+                                    value: product.amount
+                                }
                             }
-                        }
-                    ]
-                })
-            }}
+                        ]
+                    })
+                }}
                 onApprove={async (data, actions) => {
                     const order = await actions.order.capture();
                     console.log(order);
@@ -29,7 +50,9 @@ const Paypalcheckoutbutton = ({ product }) => {
                 }}
 
                 onCancel={() => { }}
-                onError={() => { }}
+                onError={(err) => {
+                    setErr(err);
+                }}
             />
         </PayPalScriptProvider>
     )
